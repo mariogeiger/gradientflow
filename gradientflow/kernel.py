f61f83cecb075cade35ec7f48f3b12d25ab2256c
Mario Geiger <geiger.mario@gmail.com>
2021-03-10 15:31:41 +0100

    kernel: f0 + docs

--- gradientflow/kernel.py
+++ gradientflow/kernel.py
@@ -8,16 +8,43 @@ from collections import namedtuple
 from .flow import flow
 
 
-def gradientflow_kernel(kernel, y, loss_prim, tau=0, max_dgrad=1e-3, max_dout=1):
+def gradientflow_kernel(kernel, y, loss_prim, f0=None, tau=0, max_dgrad=1e-3, max_dout=1):
+    """gradientflow for a kernel
+
+    model: out = f0 + kernel @ alpha
+
+    Parameters
+    ==========
+    kernel : torch.Tensor
+        tensor of shape ``(p, p)``
+
+    y : torch.Tensor
+        the labels, shape ``(p, ...)``
+
+    loss_prim : function
+        loss derivative
+        signature: (output: tensor of shape (p,), labels: tensor of shape (p, ...)) -> tensor of shape (p,)
+
+    f0 : torch.Tensor, optional
+        ``(p,)`` shape tensor
+
+    tau : float
+        momentum parameter
+
+    max_dgrad : float
+        constraint for the adaptative time step
+
+    max_dout : float
+        constraint for the adaptative time step
     """
-    gradientflow
-    model: out = kernel @ alpha
-    """
+    if f0 is None:
+        f0 = kernel.new_zeros(len(y))
+
     State = namedtuple('State', 'alpha, velo')
     Data = namedtuple('Data', 'output, gradient')
 
     def prepare(sta: State, t: float, old_data: Data, old_t: float) -> Data:
-        out = kernel @ sta.alpha
+        out = f0 + kernel @ sta.alpha
         grad = loss_prim(out, y) / len(y)
         return Data(out, grad)
 
